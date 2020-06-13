6cbdbc6b97ffcedb04acf743d966a0e5b6806cc4
Arman <Arm.amiryan1994@mail.ru>
2020-06-13 11:05:05 +0400

    Add new lesson in js-class

--- src/classes.js
+++ src/classes.js
@@ -1,4 +1,4 @@
-// Класс: базовый синтаксис
+// // Класс: базовый синтаксис
 
 // class User {
 //   constructor(name) {
@@ -12,22 +12,22 @@
 // const user = new User('Stiv');
 // user.sayHi();
 
-// Наследование классов
+// // Наследование классов
 
 // class Animal {
-// 	constructor(name) {
-// 		this.name = name;
-// 		this.speed = 0;
-// 	}
+//   constructor(name) {
+// 	this.name = name;
+// 	this.speed = 0;
+//   }
 
-// 	run(speed) {
-// 		this.speed = speed;
-//         console.log(`${this.name} run is a ${this.speed}`)
-// 	}
+//   run(speed) {
+// 	this.speed = speed;
+//     console.log(`${this.name} run is a ${this.speed}`)
+//   }
 
-// 	stop() {
-// 		console.log(`${this.name} stoped`)
-// 	}
+//   stop() {
+// 	console.log(`${this.name} stoped`)
+//   }
 // } 
 
 // const animal = new Animal('my animal');
@@ -35,70 +35,19 @@
 
 
 // class Rabbit extends Animal {
-// 	constructor(name, earLength) {
-// 		super(name);
-// 		this.earLength = earLength;
-// 	}
+//   constructor(name, earLength) {
+// 	super(name);
+// 	this.earLength = earLength;
+//   }
 // }
 
 // const rabbit = new Rabbit('black rabbit', 10);
 
 
-// class Machine {
-//   constructor(vat) {
-//     this.vat = vat;
-//   }
-
-//   setWather(value = 200) {
-//     this.water = value;
-//   }
-
-//   start(timer) {
-//     if(this.water && timer) {
-//       console.log('start')
-//       this.power = true;
-//       this.timerId = setTimeout(this.stop, timer);
-//       this.water = 0;
-//       return 'ise Ready';
-//     }
-//     console.log('not a wather');
-//   }
-
-//   stop = () => {
-//     if(this.power) {
-//       console.log('stop');
-//       this.power = false;
-//       clearTimeout(this.timerId);
-//       return;
-//     }
-//     console.log('Machin not started');
-//   }
-// };
-
-// class CoffeeMachine extends Machine {
-//   constructor(vat) {
-//     super(vat);
-//   }
-
-//   setCoffee(value) {
-//     this.coffee = value;
-//   }
-
-//   start(timer) {
-//     if(this.coffee) {
-//       super.start(timer);
-//       return;
-//     }
-//     console.log('not started', '>>>>>');
-//   }
-// }
-
-// const coffeeMachine = new CoffeeMachine(200)
-
 class Machine{
-	constructor(name) {
-		this.name = name;
-	}
+  constructor(name) {
+	this.name = name;
+  }
 
   setWater(value = 200) {
   	this.water = value;
@@ -111,7 +60,7 @@ class Machine{
   	}
   	else{
   	console.log('not start, scored a water')
-  }
+    }
   }
 
   stop() {
@@ -120,28 +69,26 @@ class Machine{
 }
 
 class CoffeeMachine extends Machine{
-	constructor(name){
-		super(name);
-	}
+  constructor(name){
+	super(name);
+  }
 
-	setCoffe(val = 50){
-     this.coffe = val;
-	}
+  setCoffe(val = 50){
+    this.coffe = val;
+  }
 
-	start() {
-		if(this.coffe){
-		this.coffe = 0;
-		super.start();
-	} else {
-		console.log('not start, Add to coffe and water')
-	}
-	}
+  start() {
+	if(this.coffe){
+	this.coffe = 0;
+	super.start();
+  } else {
+	console.log('not start, Add to coffe and water');
+  }
+  }
 
-	stop() {
-		super.stop()
-	}
-	
-  
+  stop() {
+	super.stop()
+  }
 }
 
 const coffemachine = new CoffeeMachine('coffeeMachine')
